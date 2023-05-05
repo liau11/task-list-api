601f8185129f1b8f8a0737634a661b7025e46c6e
Lily <aulily09@gmail.com>
2023-05-04 22:12:10 -0700

    Functional POST endpoint

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,28 @@ from app.models.task import Task
 from app import db
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(
+        title=request_body["title"],
+        description=request_body["description"],
+        completed_at=request_body["completed_at"],
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(
+        {
+            "task": {
+                "id": new_task.task_id,
+                "title": new_task.title,
+                "description": new_task.description,
+                "is_complete": False,
+            }
+        },
+        201,
+    )
