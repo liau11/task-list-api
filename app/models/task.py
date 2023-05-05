fe961bc78b99021c9e0e0ad3c2769ad99c2f7efd
Lily <aulily09@gmail.com>
2023-05-04 21:58:01 -0700

    Created helper function to return it in dictionary structure

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,13 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        task_as_dict = {
+            "task_id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "completed_at": self.completed_at,
+        }
+
+        return task_as_dict
