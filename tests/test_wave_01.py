d8d5f1edb571a76ea377bd437d6e38115558e0de
Lily <aulily09@gmail.com>
2023-05-06 22:07:19 -0700

    Added assert statement to test 404 error for test_update_task_not_found()

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -121,7 +121,7 @@ def test_update_task(client, one_task):
     assert task.completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_update_task_not_found(client):
     # Act
     response = client.put(
@@ -135,11 +135,7 @@ def test_update_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
+    assert response_body == {"message": "Task 1 not found"}
 
 
 # @pytest.mark.skip(reason="No way to test this feature yet")
