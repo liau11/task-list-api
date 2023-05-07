df457feaea8b3a7080acf7ccd1a7d7fc532689de
Lily <aulily09@gmail.com>
2023-05-06 21:32:45 -0700

    DELETE endpoint for specific id: restructured data that is returned

--- app/routes.py
+++ app/routes.py
@@ -80,4 +80,6 @@ def delete_task(task_id):
     db.session.delete(task)
     db.session.commit()
 
-    return make_response(f'Task {task.task_id} "{task.title}" successfully deleted')
+    return make_response(
+        {"details": 'Task {task.task_id} "{task.title}" successfully deleted'}, 200
+    )
