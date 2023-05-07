8f5835a29d9ab7116d8744f41f4a5c745228a993
Lily <aulily09@gmail.com>
2023-05-06 22:52:49 -0700

    Uncommented, also did something with merging that made my new written tests disappear...

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -5,7 +5,7 @@ from app.models.task import Task
 import pytest
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_on_incomplete_task(client, one_task):
     # Arrange
     """
@@ -36,13 +36,13 @@ def test_mark_complete_on_incomplete_task(client, one_task):
             "id": 1,
             "title": "Go on my daily walk 🏞",
             "description": "Notice something new every day",
-            "is_complete": True
+            "is_complete": True,
         }
     }
     assert Task.query.get(1).completed_at
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_on_complete_task(client, completed_task):
     # Act
     response = client.patch("/tasks/1/mark_incomplete")
@@ -56,13 +56,13 @@ def test_mark_incomplete_on_complete_task(client, completed_task):
             "id": 1,
             "title": "Go on my daily walk 🏞",
             "description": "Notice something new every day",
-            "is_complete": False
+            "is_complete": False,
         }
     }
     assert Task.query.get(1).completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_on_completed_task(client, completed_task):
     # Arrange
     """
@@ -93,13 +93,13 @@ def test_mark_complete_on_completed_task(client, completed_task):
             "id": 1,
             "title": "Go on my daily walk 🏞",
             "description": "Notice something new every day",
-            "is_complete": True
+            "is_complete": True,
         }
     }
     assert Task.query.get(1).completed_at
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_on_incomplete_task(client, one_task):
     # Act
     response = client.patch("/tasks/1/mark_incomplete")
@@ -113,7 +113,7 @@ def test_mark_incomplete_on_incomplete_task(client, one_task):
             "id": 1,
             "title": "Go on my daily walk 🏞",
             "description": "Notice something new every day",
-            "is_complete": False
+            "is_complete": False,
         }
     }
     assert Task.query.get(1).completed_at == None
