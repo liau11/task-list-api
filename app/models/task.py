73965be7b36f565c22874787679bb47114a86ec0
Lily <aulily09@gmail.com>
2023-05-06 22:47:48 -0700

    Changed some variable names, also casted bool on "is_complete" for PATCH

--- app/models/task.py
+++ app/models/task.py
@@ -9,10 +9,10 @@ class Task(db.Model):
 
     def to_dict(self):
         task_as_dict = {
-            "task_id": self.task_id,
+            "id": self.task_id,
             "title": self.title,
             "description": self.description,
-            "completed_at": self.completed_at,
+            "is_complete": bool(self.completed_at),
         }
 
         return task_as_dict
