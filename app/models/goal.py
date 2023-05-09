6f275e8e204dbf7fe4d9004f84772c4a7cb943c9
Lily <aulily09@gmail.com>
2023-05-08 19:05:37 -0700

    return goal as dictionary

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,12 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+
+
+def to_dict(self):
+    goal_as_dict = {
+        "id": self.goal_id,
+        "title": self.title,
+    }
+
+    return goal_as_dict
