de272c2f9ef77ec18db7e3ca62426df8a3f39485
Lily <aulily09@gmail.com>
2023-05-08 21:01:52 -0700

    DELETE by id endpoint functional

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -42,3 +42,16 @@ def read_one_goal(goal_id):
     goal = get_valid_item_by_id(Goal, goal_id)
 
     return make_response({"goal": goal.to_dict()}, 200)
+
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = get_valid_item_by_id(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response(
+        {"details": f'Goal {goal_id} "{goal.title}" successfully deleted'},
+        200,
+    )
