53fcd5eff710c17b3ef102709da097e190c38eff
Lily <aulily09@gmail.com>
2023-05-08 21:12:30 -0700

    wrote assert statements for test_update_goal

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_get_goals_no_saved_goals(client):
     # Act
     response = client.get("/goals")
@@ -12,7 +12,7 @@ def test_get_goals_no_saved_goals(client):
     assert response_body == []
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_get_goals_one_saved_goal(client, one_goal):
     # Act
     response = client.get("/goals")
@@ -24,7 +24,7 @@ def test_get_goals_one_saved_goal(client, one_goal):
     assert response_body == [{"id": 1, "title": "Build a habit of going outside daily"}]
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_get_goal(client, one_goal):
     # Act
     response = client.get("/goals/1")
@@ -38,7 +38,7 @@ def test_get_goal(client, one_goal):
     }
 
 
-@pytest.mark.skip(reason="test to be completed by student")
+# @pytest.mark.skip(reason="test to be completed by student")
 def test_get_goal_not_found(client):
     pass
     # Act
@@ -49,7 +49,7 @@ def test_get_goal_not_found(client):
     assert response_body == {"message": "Goal 1 not found"}
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_goal(client):
     # Act
     response = client.post("/goals", json={"title": "My New Goal"})
@@ -61,18 +61,20 @@ def test_create_goal(client):
     assert response_body == {"goal": {"id": 1, "title": "My New Goal"}}
 
 
-@pytest.mark.skip(reason="test to be completed by student")
+# @pytest.mark.skip(reason="test to be completed by student")
 def test_update_goal(client, one_goal):
-    raise Exception("Complete test")
     # Act
-    # ---- Complete Act Here ----
-
+    response = client.put(
+        "/goals/1",
+        json={
+            "title": "Updated Goal Title",
+        },
+    )
+    response_body = response.get_json()
     # Assert
-    # ---- Complete Assertions Here ----
-    # assertion 1 goes here
-    # assertion 2 goes here
-    # assertion 3 goes here
-    # ---- Complete Assertions Here ----
+    assert response.status_code == 200
+    assert "goal" in response_body
+    assert response_body == {"goal": {"id": 1, "title": "Updated Goal Title"}}
 
 
 @pytest.mark.skip(reason="test to be completed by student")
