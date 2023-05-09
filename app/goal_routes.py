6def4c3ce8a3519c7bd3058edf0c186775c13434
Lily <aulily09@gmail.com>
2023-05-08 19:51:49 -0700

    fixded db to bp. Also GET endpoint for all books

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -4,10 +4,10 @@ from app.models.goal import Goal
 from app import db
 
 
-goals_db = Blueprint("tasks", __name__, url_prefix="/goals")
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
 
-@goals_db.route("", methods=["POST"])
+@goals_bp.route("", methods=["POST"])
 def create_goal():
     request_body = request.get_json()
 
@@ -20,3 +20,17 @@ def create_goal():
     db.session.commit()
 
     return make_response({"goal": new_goal.to_dict()}, 201)
+
+
+@goals_bp.route("", methods=["GET"])
+def read_all_goals():
+    sort_method = request.args.get("sort")
+
+    if sort_method == "desc":
+        goals = Goal.query.order_by(Goal.title.desc()).all()
+    elif sort_method == "asc":
+        goals = Goal.query.order_by(Goal.title.asc()).all()
+    else:
+        goals = Goal.query.all()
+
+    return jsonify([goal.to_dict() for goal in goals])
