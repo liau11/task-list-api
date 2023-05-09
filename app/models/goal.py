2e1ada0c35c857a053ccef3284939b0ec70d46fe
Lily <aulily09@gmail.com>
2023-05-08 20:07:44 -0700

    indented methods

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,17 +5,15 @@ class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
 
+    def to_dict(self):
+        goal_as_dict = {
+            "id": self.goal_id,
+            "title": self.title,
+        }
 
-def to_dict(self):
-    goal_as_dict = {
-        "id": self.goal_id,
-        "title": self.title,
-    }
+        return goal_as_dict
 
-    return goal_as_dict
-
-
-@classmethod
-def from_dict(cls, goal_data):
-    new_goal = Goal(title=goal_data["title"])
-    return new_goal
+    @classmethod
+    def from_dict(cls, goal_data):
+        new_goal = Goal(title=goal_data["title"])
+        return new_goal
